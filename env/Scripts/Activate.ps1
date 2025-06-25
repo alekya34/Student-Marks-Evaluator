8d15a450374f77d9aa3fd5a36ac554b71414ecc2
alekya34 <manthrialekya@gmail.com>
2025-06-25 19:00:25 +0530

    Fix: renamed Templates to templates

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\vasu\Desktop\Student-Marks-Evalutor\env"
+$env:VIRTUAL_ENV="C:\Users\vasu\Desktop\Student-Marks-Evaluator\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
